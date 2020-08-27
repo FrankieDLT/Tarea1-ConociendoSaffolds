a5d1e8a2e95a30627c2c513567f423324cf2f7c6
FrankieDLT <51407226+FrankieDLT@users.noreply.github.com>
2020-08-27 17:38:31 -0500

    Tarea Terminada
    Se terminaron todos los requerimientos que se pidieron en la tarea, se aexa tambien el video de la funcionalidad.

--- ConSca/lib/main.dart
+++ ConSca/lib/main.dart
@@ -22,14 +22,12 @@ class Homiespage extends StatefulWidget {
 }
 
 class _HomiespageState extends State<Homiespage> {
-  final _textController = TextEditingController();
-  bool _isActive = true;
   bool _isseen = true;
   int _counter = 0;
 
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    var _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -48,31 +46,90 @@ class _HomiespageState extends State<Homiespage> {
               }),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("Número actual de clicks es: ${_counter}"),
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  _counter++;
-                }); 
-                _scaffoldKey.currentState
-                  ..hideCurrentSnackBar()
-                  ..showSnackBar(
-                    SnackBar(
-                      content: Text("HI"),
-                    ),
-                  );
-              }, //On pressed
-              color: Colors.purple,
-              child: Text(
-                "Click me bitch",
-                style: TextStyle(color: Colors.white),
-              ),
+      body: Container(
+        
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/gal.jpg"),
+            fit: BoxFit.cover,
             ),
-          ],
+        ),
+        child: Center(
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Número actual de clicks es: ${_counter}",
+                style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter++;
+                    });
+
+                    if (_counter % 2 == 0) {
+                      _scaffoldKey.currentState
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(
+                          SnackBar(
+                            content: Text("Par"),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                    } else {
+                      _scaffoldKey.currentState //.hideCurrentSnackBar();
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(
+                          SnackBar(
+                            content: Text("Impar"),
+                            duration: Duration(seconds: 3),
+                            action: SnackBarAction(
+                              label: "I´m a button :D",
+                              onPressed: () {
+                                DateTime _time = DateTime.now();
+                                String _fecha = "${_time.day.toString().padLeft(2,'0')}/${_time.month.toString().padLeft(2,'0')}/${_time.year.toString()}";
+                                String _hora = "${_time.hour.toString()}:${_time.minute.toString()}";
+                                //Mostrar Dialogo
+                                showDialog(
+                                  context: context,
+                                  builder: (_) {
+                                    return AlertDialog(
+                                      title: Text("Fecha y Hora Actual:"),
+                                      content: SingleChildScrollView(
+                                        child: ListBody(
+                                          children: <Widget>[
+                                            Text("Fecha: ${_fecha}"),
+                                            Text("Hora: ${_hora}"),
+                                          ],
+                                        ),
+                                      ),
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text("Salir"),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              }, //Onpressed
+                            ),
+                          ),
+                        );
+                    }
+                  }, //On pressed
+                  color: Colors.purple,
+                  child: Text(
+                    "Click me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
